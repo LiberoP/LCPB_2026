88fefec163b580c9342d0b6bfba434f2ff539fd3
LiberoP <pollinilibero@gmail.com>
2026-08-09 00:46:39 +0200

    Updated general analysis, dataset cleaning and splitting defined as functions in .py, first MACE training (trains fast, seems ok, but not tested yet)

---
+++ Project/useful_functions.py
@@ -0,0 +1,55 @@
+# from ASE.py:
+# reads local OUTCAR file for analysis (see "general analysis" notebook)
+import ase.io
+from ase.io import read, write
+import numpy as np
+
+
+def clean_dataset(outcar_file, destination_file, start_idx):
+    try:
+        atoms_list = read(outcar_file, index=":")
+        atoms_list = atoms_list[start_idx:]
+        print(f"Retrieved {len(atoms_list)} structures from OUTCAR.")
+
+        # # --- Add energy and forces to each Atoms object ---
+        # for atoms in atoms_list:
+        #     atoms.info["energy"] = atoms.get_potential_energy()  # total energy (eV)
+        #     atoms.arrays["forces"] = atoms.get_forces()  # (N_atoms,3) in eV/Å
+
+        # usable file
+        write(destination_file, atoms_list, format="extxyz")
+        print(f"File saved as {destination_file}")
+
+    except Exception as e:
+        print(f"Error cleaning OUTCAR: {e}")
+
+    return 0
+
+
+# from split.py:
+# add seed??
+def split_train_valid(
+    full_cleaned_extxyz_file, destination_train_file, destination_valid_file, train_frac, seed=1
+):
+    traj = ase.io.read(full_cleaned_extxyz_file, index=":")
+    print(f"Total frames uploaded: {len(traj)}")
+
+    #np.random.shuffle(traj)
+    rng = np.random.RandomState(seed)
+    indices = np.arange(len(traj))
+    rng.shuffle(indices)
+    traj = [traj[i] for i in indices]
+
+
+    split = int(train_frac * len(traj))
+    train_frames = traj[:split]
+    valid_frames = traj[split:]
+
+    ase.io.write(destination_train_file, train_frames)
+    ase.io.write(destination_valid_file, valid_frames)
+
+    print(
+        f"Saved {len(train_frames)} frames in {destination_train_file} and {len(valid_frames)} in {destination_valid_file}"
+    )
+
+    return 0
